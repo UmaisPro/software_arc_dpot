90a1116a66d510f2fd4376f52068428f796dcef9
UmaisPro <mughalumais05@gmail.com>
2025-01-07 10:00:51 +0500

    model classes created

---
+++ src/model/Log.java
@@ -0,0 +1,32 @@
+package model;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class Log {
+    private static Log instance;
+    private StringBuffer logBuffer;
+
+    private Log() {
+        logBuffer = new StringBuffer();
+    }
+
+    public static Log getInstance() {
+        if (instance == null) {
+            instance = new Log();
+        }
+        return instance;
+    }
+
+    public void addLog(String event) {
+        logBuffer.append(event).append("\n");
+    }
+
+    public void saveLogToFile(String filename) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename))) {
+            writer.write(logBuffer.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
